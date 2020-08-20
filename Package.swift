cd08284ec895090acfa78ad61af0bb39a453ae08
schlagelk <kenny.schlagel+github@gmail.com>
2020-08-19 20:16:12 -0600

    SAP version bump

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "mariachi",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
